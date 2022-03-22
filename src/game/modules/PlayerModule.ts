5b651d4b0ca842a1926a3decc1bbcfabd59c2756
tobiasreinbold linux <tobiasreinbold1@gmail.com>
2022-03-23 00:54:35 +0100

    setup PlayerModule

---
+++ src/game/modules/PlayerModule.ts
@@ -0,0 +1,19 @@
+import Entity from "src/engine/ecs/Entity";
+import EntityManager from "src/engine/ecs/EntityManager";
+import Module from "src/engine/ecs/Module";
+import Observable from "src/lib/Observable";
+import { Quaternion, Vector3 } from "three";
+
+class PlayerModule extends Module {
+  constructor() {
+    super();
+  }
+
+  Define(entityManager: EntityManager): void {
+    const entity = new Entity("Player");
+    entity.AddObservable("position", new Observable(new Vector3()));
+    entity.AddObservable("rotation", new Observable(new Quaternion()));
+  }
+}
+
+export default PlayerModule;
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import Game from "src/engine/ecs/Game";
 import WorldModule from "src/game/modules/WorldModule";
 import "src/style.css";
+import PlayerModule from "./game/modules/PlayerModule";
 
 const gameElement = document.getElementById("game");
 if (gameElement) {
-  new Game().AddModule(new WorldModule(gameElement));
+  new Game()
+    .AddModule(new WorldModule(gameElement))
+    .AddModule(new PlayerModule());
 } else {
   console.log("couldnt get element");
 }
